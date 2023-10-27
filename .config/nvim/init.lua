3fd38a020be5759d8ac1fe3a61f49addc3c2f08e
tomotargz <nakamuratomota@gmail.com>
2023-10-27 13:13:37 +0900

    mod

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -25,7 +25,7 @@ vim.opt.swapfile = false
 
 -- Other
 vim.opt.hidden = true
-vim.opt.clipboard = 'unnamedplus' --クリップボードとレジスタを共有
+vim.opt.clipboard = 'unnamedplus'
 
 -- Encode
 vim.scriptencoding = 'utf-8'
@@ -85,6 +85,7 @@ require("lazy").setup({
         dependencies = { 'nvim-lua/plenary.nvim' },
     },
     'glidenote/memolist.vim',
+    'terrortylor/nvim-comment',
 })
 
 require('mason').setup {
@@ -176,3 +177,18 @@ vim.keymap.set('n', '<C-g>', builtin.live_grep, {})
 
 -- Colorscheme
 vim.cmd[[colorscheme kuro]]
+
+require('nvim_comment').setup()
+
+-- vim.keymap.set('n', 'K',  '<cmd>lua vim.lsp.buf.hover()<CR>')
+vim.keymap.set('n', '<leader>f', '<cmd>lua vim.lsp.buf.format()<CR>')
+-- vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>')
+vim.keymap.set('n', '<C-]>', '<cmd>lua vim.lsp.buf.definition()<CR>')
+-- vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>')
+-- vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>')
+-- vim.keymap.set('n', 'gt', '<cmd>lua vim.lsp.buf.type_definition()<CR>')
+-- vim.keymap.set('n', 'gn', '<cmd>lua vim.lsp.buf.rename()<CR>')
+-- vim.keymap.set('n', 'ga', '<cmd>lua vim.lsp.buf.code_action()<CR>')
+-- vim.keymap.set('n', 'ge', '<cmd>lua vim.diagnostic.open_float()<CR>')
+-- vim.keymap.set('n', 'g]', '<cmd>lua vim.diagnostic.goto_next()<CR>')
+-- vim.keymap.set('n', 'g[', '<cmd>lua vim.diagnostic.goto_prev()<CR>')
