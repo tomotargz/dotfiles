2dc3f467151b639bbb47dccfd42bd12001e4ac3d
tomotargz <nakamuratomota@gmail.com>
2023-11-12 15:58:13 +0900

    Mod

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,57 @@
+-- View
+vim.opt.wrap = false
+vim.opt.smartindent = true
+vim.opt.laststatus = 2
+vim.opt.colorcolumn = '80'
+vim.opt.number = false
+
+-- Tab
+vim.opt.smarttab = true
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+
+-- Search
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+vim.opt.smartcase = true
+vim.opt.ignorecase = true
+
+-- Temp files
+vim.opt.backup = false
+vim.opt.undofile = false
+vim.opt.swapfile = false
+
+-- Other
+vim.opt.hidden = true
+vim.opt.clipboard = 'unnamedplus'
+
+-- Encode
+vim.scriptencoding = 'utf-8'
+vim.opt.encoding = 'utf-8'
+vim.opt.fileencoding = 'utf-8'
+
+-- Keymap
+vim.g.mapleader = ' '
+vim.keymap.set('n', '<leader>w', ':w<Return>')
+vim.keymap.set('n', '<leader>h', ':noh<Return>')
+vim.keymap.set('n', '<leader>t', ':vsplit|terminal<Return>')
+vim.keymap.set('n', '<leader>v', ':e $MYVIMRC<Return>')
+vim.keymap.set('n', '<leader>vv', ':source $MYVIMRC<Return>')
+vim.keymap.set('n', 'j', 'gj')
+vim.keymap.set('n', 'k', 'gk')
+vim.keymap.set('n', 'Y', 'y$')
+vim.keymap.set('i', 'jj', '<Esc>')
+vim.keymap.set('t', '<C-w>h', '<C-\\><C-n><C-w>h')
+vim.keymap.set('t', '<C-w>j', '<C-\\><C-n><C-w>j')
+vim.keymap.set('t', '<C-w>k', '<C-\\><C-n><C-w>k')
+vim.keymap.set('t', '<C-w>l', '<C-\\><C-n><C-w>l')
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
+
 -- Plugin
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
@@ -112,57 +166,3 @@ require("lazy").setup({
         end
     }
 })
-
--- View
-vim.opt.wrap = false
-vim.opt.smartindent = true
-vim.opt.laststatus = 2
-vim.opt.colorcolumn = '80'
-vim.opt.number = false
-
--- Tab
-vim.opt.smarttab = true
-vim.opt.expandtab = true
-vim.opt.shiftwidth = 4
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-
--- Search
-vim.opt.hlsearch = true
-vim.opt.incsearch = true
-vim.opt.smartcase = true 
-vim.opt.ignorecase = true 
-
--- Temp files
-vim.opt.backup = false
-vim.opt.undofile = false
-vim.opt.swapfile = false
-
--- Other
-vim.opt.hidden = true
-vim.opt.clipboard = 'unnamedplus'
-
--- Encode
-vim.scriptencoding = 'utf-8'
-vim.opt.encoding = 'utf-8'
-vim.opt.fileencoding = 'utf-8'
-
--- Keymap
-vim.g.mapleader = ' '
-vim.keymap.set('n', '<leader>w', ':w<Return>')
-vim.keymap.set('n', '<leader>h', ':noh<Return>')
-vim.keymap.set('n', '<leader>t', ':vsplit|terminal<Return>')
-vim.keymap.set('n', '<leader>v', ':e $MYVIMRC<Return>')
-vim.keymap.set('n', '<leader>vv', ':source $MYVIMRC<Return>')
-vim.keymap.set('n', 'j', 'gj')
-vim.keymap.set('n', 'k', 'gk')
-vim.keymap.set('n', 'Y', 'y$')
-vim.keymap.set('i', 'jj', '<Esc>')
-vim.keymap.set('t', '<C-w>h', '<C-\\><C-n><C-w>h')
-vim.keymap.set('t', '<C-w>j', '<C-\\><C-n><C-w>j')
-vim.keymap.set('t', '<C-w>k', '<C-\\><C-n><C-w>k')
-vim.keymap.set('t', '<C-w>l', '<C-\\><C-n><C-w>l')
-vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
