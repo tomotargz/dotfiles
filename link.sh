7eb7532d92a73480fa807028b5e8fa8de899c757
Tomota Nakamura <tnakamur@redhat.com>
2025-12-05 16:55:16 +0900

    mod

--- link.sh
+++ link.sh
@@ -6,6 +6,8 @@ if ! command -v stow >/dev/null 2>&1; then
     exit 1
 fi
 
+[ -d "$HOME/.config" ] || mkdir -p "$HOME/.config"
+
 for dir in $(find . -mindepth 1 -maxdepth 1 -type d ! -path "./.*") ; do
     dir="${dir#./}"
     echo "stowing $dir"
